ecf5a505d8bf7a51aa900e9e96c1d877d2303a10
mikigo <huangmingqiang@uniontech.com>
2024-01-12 17:56:47 +0800

    update

--- env.sh
+++ env.sh
@@ -1,7 +1,5 @@
-sudo apt install python3-pip
-sudo pip3 install -U pip -i https://pypi.tuna.tsinghua.edu.cn/simple
-sudo pip3 install pipenv -i https://pypi.tuna.tsinghua.edu.cn/simple
-pipenv --python 3
-pipenv run pip install -r requirements.txt
-env_path=$(pipenv --venv)
-cp -r mkdocs_toc_plugin ${env_path}
+npm install -g pnpm
+pnpm add -D vitepress
+pnpm i vitepress-plugin-comment-with-giscus
+pnpm i @giscus/vue
+pnpm i vite-plugin-vitepress-auto-sidebar
